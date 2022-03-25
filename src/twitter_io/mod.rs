032c30257bb5ce41517341bfb425bf029a6b37ab
pistachio <pistaio@protonmail.com>
2022-03-25 11:46:51 +0530

    Handled edge case in refresh token flow; Improved code

--- src/twitter_io/mod.rs
+++ src/twitter_io/mod.rs
@@ -56,6 +56,12 @@ struct Data {
     text: String
 }
 
+#[derive(Debug)]
+struct Token {
+    access: oauth2::AccessToken,
+    refresh: oauth2::RefreshToken 
+}
+
 pub fn tweet_quote(quote: String) {
     if !Path::new(TOKENS_PATH).exists() {
         thread::spawn(move || {
@@ -70,16 +76,24 @@ pub fn tweet_quote(quote: String) {
         let contents = fs::read_to_string(TOKENS_PATH)
             .expect("Something went wrong reading the file");
 
-        let tokens: Vec<&str> = contents.split("\n").collect();
+        let mut tokens_str: Vec<&str> = contents.split("\n").collect();
+        tokens_str.retain(|x| *x != "");
+        assert_eq!(tokens_str.len(), 2);
+
+        let tokens = Token {
+            access: oauth2::AccessToken::new(tokens_str[0].to_string()),
+            refresh: oauth2::RefreshToken::new(tokens_str[1].to_string())
+        };
+
         let mut index = 0;
-        let mut tweet_id = handle_post_tweet_response(tokens[0].to_owned(), tokens[1].to_owned(), tweets[index].to_owned(), None);
+        let mut response  = handle_post_tweet_response(tokens, tweets[index].to_owned(), None);
         index += 1;
         while index < tweets.len() {
             let reply = TweetReply {
-                in_reply_to_tweet_id: tweet_id 
+                in_reply_to_tweet_id: response.0 
             };
 
-            tweet_id = handle_post_tweet_response(tokens[0].to_owned(), tokens[1].to_owned(), tweets[index].to_owned(), Some(reply));
+            response = handle_post_tweet_response(response.1, tweets[index].to_owned(), Some(reply));
             index += 1;
         }
 
@@ -90,18 +104,16 @@ pub fn tweet_quote(quote: String) {
     }
 }
 
-fn handle_post_tweet_response(access_token: String, refresh_token: String, tweet: String, tweet_id: Option<TweetReply>) -> String {
-    match post_tweet(access_token.clone(), refresh_token.clone(), tweet.clone(), tweet_id.clone()) {
-        Ok(new_id) => new_id,
+fn handle_post_tweet_response(token: Token, tweet: String, tweet_id: Option<TweetReply>) -> (String, Token) {
+    match post_tweet(token, tweet.clone(), tweet_id.clone()) {
+        Ok(response) => response,
         Err(e) => { panic!("Error: {}", e) }
     }
 }
 
 // TODO: Test refresh token flow. It should fetch new tokens and post tweet in a single go. 
-// #[async_recursion(?Send)]
-fn post_tweet(access_token: String, refresh_token: String, 
-                    tweet: String, tweet_id: Option<TweetReply>) 
-    -> Result<String, Box<dyn std::error::Error>> {
+fn post_tweet(token: Token, tweet: String, tweet_id: Option<TweetReply>) 
+    -> Result<(String, Token), Box<dyn std::error::Error>> {
 
     let endpoint = "https://api.twitter.com/2/tweets";
 
@@ -116,7 +128,7 @@ fn post_tweet(access_token: String, refresh_token: String,
     let client = reqwest::blocking::Client::new();
     let request = client
                     .post(endpoint)
-                    .bearer_auth(&access_token)
+                    .bearer_auth(token.access.secret())
                     .headers(headers)
                     .json(&body);
 
@@ -128,13 +140,15 @@ fn post_tweet(access_token: String, refresh_token: String,
             let response_body: TweetResponse = response.json()?;
             let id = response_body.data.id;
             println!("posted tweet id: {}", id);
-            Ok(id)
+            // Pass new access_token and refresh_token incase of refresh token flow
+            // was previously run.
+            Ok((id, token))
         },
         reqwest::StatusCode::UNAUTHORIZED => {
             // Run refresh token flow
             println!("----------------Here we are (unauthorized)----------------");
-            match refresh_access_token(&refresh_token) {
-                Ok(tok) => post_tweet(tok.0, tok.1, tweet, tweet_id),
+            match refresh_access_token(token.refresh) {
+                Ok(token) => post_tweet(token, tweet, tweet_id),
                 Err(e) => return Err(e)
             }
         },
@@ -145,20 +159,19 @@ fn post_tweet(access_token: String, refresh_token: String,
     }
 }
 
-fn refresh_access_token(refresh_token: &str) -> Result<(String, String), Box<dyn std::error::Error>>{
+fn refresh_access_token(refresh_token: RefreshToken) -> Result<Token, Box<dyn std::error::Error>>{
     let client_id = ClientId::new(CLIENT_ID.to_string());
     let client_secret = ClientSecret::new(CLIENT_SECRET.to_string());
     let auth_url = AuthUrl::new("https://twitter.com/i/oauth2/authorize".to_string())
         .unwrap();
     let token_url = TokenUrl::new("https://api.twitter.com/2/oauth2/token".to_string())
         .unwrap();
-    let token = RefreshToken::new(refresh_token.to_string());
 
     let client = BasicClient::new(client_id, Some(client_secret), auth_url, Some(token_url))
         .set_auth_type(AuthType::BasicAuth);
 
     let token_result = client
-        .exchange_refresh_token(&token)
+        .exchange_refresh_token(&refresh_token)
         .request(http_client)?;
 
     println!("{:#?}", token_result);
@@ -166,12 +179,18 @@ fn refresh_access_token(refresh_token: &str) -> Result<(String, String), Box<dyn
     let access_token = token_result.access_token();
     let refresh_token = token_result.refresh_token().unwrap(); // twitter always sends refresh token
 
-    println!("{}", access_token.secret());
-    println!("{}", refresh_token.secret());
+    // println!("{}", access_token.secret());
+    // println!("{}", refresh_token.secret());
 
     save_tokens_to_file(access_token, refresh_token);
 
-    Ok((access_token.secret().clone(), refresh_token.secret().clone()))
+    let token = Token {
+        access: access_token.clone(),
+        refresh: refresh_token.clone()
+    };
+
+    Ok(token)
+    // Ok((access_token.secret().clone(), refresh_token.secret().clone()))
 }
 
 
